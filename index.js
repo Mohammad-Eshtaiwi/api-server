f00ac317f260c42fcee084ad6cf94612291ec5c6
Mohammad-Eshtaiwi <mohd.eshtiwi@gmail.com>
2020-10-21 08:33:40 +0300

    class 8

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-require('dotenv');
-
+require('dotenv').config();
+const mongoose = require('mongoose');
 const server = require('./lib/server');
+mongoose.connect('mongodb://localhost/api-server', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 server.start(3000);
